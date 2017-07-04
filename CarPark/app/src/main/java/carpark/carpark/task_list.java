05a8a1509af2ba9aa1a775933ce5911bb9125029
dronkrut96 <syndikat_dron1996@mail.ru>
2017-07-04 14:17:14 +0700

    Подправил OnClick

--- CarPark/app/src/main/java/carpark/carpark/task_list.java
+++ CarPark/app/src/main/java/carpark/carpark/task_list.java
@@ -1,12 +1,9 @@
 package carpark.carpark;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
 public class task_list extends AppCompatActivity {
 
@@ -16,6 +13,6 @@ public class task_list extends AppCompatActivity {
         setContentView(R.layout.task_list);
     }
 
-    public void MA3(View view) { startActivity(new Intent(this, full_information.class)); }
+    public void full_information(View view) { startActivity(new Intent(this, full_information.class)); }
 
 }
