41635303bf07cbe010ef168ca8e5c5acc90503ec
zabelyaev <zabelyaev@gmail.com>
2017-07-05 16:18:18 +0800

    Bred

--- CarPark/app/src/main/java/carpark/carpark/add_task.java
+++ CarPark/app/src/main/java/carpark/carpark/add_task.java
@@ -2,8 +2,10 @@ package carpark.carpark;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class add_task extends AppCompatActivity {
+    EditText
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
