33b95208952372010af319b9922ac5df255c2df4
dronkrut96 <syndikat_dron1996@mail.ru>
2017-07-06 20:31:25 +0700

    Допилен интерфейс добавления задания. Кнопка добавления перенесена в actionbar.

--- CarPark/app/src/main/java/carpark/carpark/task_list_a.java
+++ CarPark/app/src/main/java/carpark/carpark/task_list_a.java
@@ -3,7 +3,8 @@ package carpark.carpark;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class task_list_a extends AppCompatActivity {
 
@@ -13,5 +14,14 @@ public class task_list_a extends AppCompatActivity {
         setContentView(R.layout.task_list_a);
     }
 
-    public void add_task(View view) { startActivity(new Intent(this, add_task.class)); }
+    public void add_task(MenuItem item) {
+        startActivity(new Intent(this, add_task.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
